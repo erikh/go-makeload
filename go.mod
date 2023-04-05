e26163dd6185faf8c3f447e60201ed510ae15033
Erik Hollensbe <git@hollensbe.org>
2023-04-04 17:27:11 -0700

    Inject goleak for leak testing, make some test tweaks that were necessary
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/erikh/go-makeload
 
 go 1.20
+
+require go.uber.org/goleak v1.2.1 // indirect
--- makeload_test.go
+++ makeload_test.go
@@ -9,20 +9,30 @@ import (
 	"runtime"
 	"testing"
 	"time"
+
+	"go.uber.org/goleak"
 )
 
 type Server struct{}
 
-func (s *Server) ServeHTTP(rw http.ResponseWriter, req *http.Request) {}
+func (s *Server) ServeHTTP(http.ResponseWriter, *http.Request) {}
 
 func TestBasic(t *testing.T) {
+	defer goleak.VerifyNone(t)
+
 	srv := &Server{}
 	l, err := net.Listen("tcp", ":0")
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	go http.Serve(l, srv)
+	httpSrv := &http.Server{
+		Handler: srv,
+	}
+
+	go httpSrv.Serve(l)
+	defer httpSrv.Shutdown(context.Background())
+	defer l.Close()
 
 	// don't annoy the tester, but allow the test to finish for large core counts.
 	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(runtime.NumCPU()*20)*time.Second)
@@ -34,9 +44,9 @@ func TestBasic(t *testing.T) {
 	}
 
 	lg := &LoadGenerator{
-		Concurrency:             uint(runtime.NumCPU() / 2),          // give room for the server to work
-		SimultaneousConnections: uint(runtime.NumCPU() * 1000),       // a very conservative value for modern processors
-		TotalConnections:        uint(runtime.NumCPU() * 1000 * 100), // roughly spoken, 100k conns * cpu count for the battery
+		Concurrency:             uint(runtime.NumCPU() / 2),      // give room for the server to work
+		SimultaneousConnections: uint(runtime.NumCPU() * 1000),   // a very conservative value for modern processors
+		TotalConnections:        uint(runtime.NumCPU() * 100000), // roughly spoken, 100k conns * cpu count for the battery
 		Ctx:                     ctx,
 		URL:                     u,
 	}
