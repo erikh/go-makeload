e3b14475568eb45b584c9199d8efc40cb60659c0
Erik Hollensbe <git@hollensbe.org>
2023-09-18 21:27:15 -0700

    introduction of benchmarker with tests
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/erikh/go-makeload
 
 go 1.20
 
-require go.uber.org/goleak v1.2.1 // indirect
+require go.uber.org/goleak v1.2.1
--- makeload_test.go
+++ makeload_test.go
@@ -17,9 +17,7 @@ type Server struct{}
 
 func (s *Server) ServeHTTP(http.ResponseWriter, *http.Request) {}
 
-func TestBasic(t *testing.T) {
-	defer goleak.VerifyNone(t)
-
+func createServer(t *testing.T) (net.Listener, *http.Server, *url.URL) {
 	srv := &Server{}
 	l, err := net.Listen("tcp", ":0")
 	if err != nil {
@@ -31,17 +29,25 @@ func TestBasic(t *testing.T) {
 	}
 
 	go httpSrv.Serve(l)
-	defer httpSrv.Shutdown(context.Background())
-	defer l.Close()
-
-	// don't annoy the tester, but allow the test to finish for large core counts.
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(runtime.NumCPU()*20)*time.Second)
-	defer cancel()
 
 	u, err := url.Parse("http://" + l.Addr().String())
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	return l, httpSrv, u
+}
+
+func TestLoadGenerator(t *testing.T) {
+	defer goleak.VerifyNone(t)
+
+	// don't annoy the tester, but allow the test to finish for large core counts.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(runtime.NumCPU()*20)*time.Second)
+	defer cancel()
+
+	l, httpSrv, u := createServer(t)
+	defer httpSrv.Shutdown(context.Background())
+	defer l.Close()
 
 	lg := NewLoadGenerator(
 		&BatteryProperties{
@@ -60,4 +66,37 @@ func TestBasic(t *testing.T) {
 	t.Log("total delivered: " + fmt.Sprintf("%d", lg.Properties.Stats.Successes+lg.Properties.Stats.Failures))
 	t.Log("successes: " + fmt.Sprintf("%d", lg.Properties.Stats.Successes))
 	t.Log("failures: " + fmt.Sprintf("%d", lg.Properties.Stats.Failures))
+}
+
+func TestBenchmarker(t *testing.T) {
+	seconds := 30
+
+	defer goleak.VerifyNone(t)
+
+	// don't annoy the tester, but allow the test to finish for large core counts.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(runtime.NumCPU()*20)*time.Second)
+	defer cancel()
+
+	l, httpSrv, u := createServer(t)
+	defer httpSrv.Shutdown(context.Background())
+	defer l.Close()
+
+	lg := NewBenchmarker(
+		&BatteryProperties{
+			Concurrency:             uint(runtime.NumCPU() / 2),    // give room for the server to work
+			SimultaneousConnections: uint(runtime.NumCPU() * 1000), // a very conservative value for modern processors
+			Ctx:                     ctx,
+			URL:                     u,
+		},
+		time.Duration(seconds)*time.Second, // roughly spoken, 100k conns * cpu count for the battery
+	)
+
+	if err := lg.Spawn(); err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log("total delivered: " + fmt.Sprintf("%d", lg.Properties.Stats.Successes+lg.Properties.Stats.Failures))
+	t.Log("successes: " + fmt.Sprintf("%d", lg.Properties.Stats.Successes))
+	t.Log("failures: " + fmt.Sprintf("%d", lg.Properties.Stats.Failures))
+	t.Log("requests/sec: " + fmt.Sprintf("%f", float64(lg.Properties.Stats.Successes)/float64(seconds)))
 }
